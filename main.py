6c23c308728acfd47224553c9cb5d0ac913fdcf4
anhede <josefanhede1@gmail.com>
2025-06-23 16:39:22 +0200

    More work on time routine

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ from components.distsensor import Distsensor
 from client.client import Client
 from client.wifi_client import WifiClient
 
+from routines.alarm import alarm
+from routines.interactive_story import interactive_story
+
 DEEP_SLEEP_MS = 10000
 SLEEP_MS = 5000
+ALARM_STATE_CHANGE_FREEZE_MS = 2000  # Prevents the alarm from going off immediately after setting it
 
 
 class AlarmState:
@@ -23,6 +27,7 @@ class AlarmState:
         self.__is_on = is_on
         self.__just_changed = True
         self.__last_change_ms = time.ticks_ms()
+        self.__last_fired_ms = time.ticks_ms() - 120 * 1000  # Set to 2 minutes ago to allow immediate firing
 
     def choice(self, choice: int | None = None):
         # Set
@@ -83,6 +88,19 @@ class AlarmState:
         """
         return time.ticks_diff(time.ticks_ms(), self.__last_change_ms)
 
+    def just_fired(self):
+        """
+        Tell the alarm that it just fired.
+        """
+        self.__last_fired_ms = time.ticks_ms()
+
+    def armed(self):
+        """
+        Is the alarm ready to go off?
+        """
+        if time.ticks_diff(time.ticks_ms(), self.__last_fired_ms) < 120 * 1000:
+            return False
+        return True
 
 def main():
     """
@@ -107,8 +125,9 @@ def main():
     sleep = False
     deep_sleep = False
     last_dist_was_close_ms = 0
+    last_time_string = ""
     while True:
-        update_alarm_state(pot, button, state)
+        update_alarm_state(pot, button, state, sleep)
 
         # Display updated settings
         if state.just_changed():
@@ -118,7 +137,10 @@ def main():
 
         if not sleep and state.ms_since_last_change() > SLEEP_MS:
             sleep = True
-            display_sleep_state(screen, state)
+            last_time_string = display_sleep_state(screen, state, last_time_string)
+
+        if not deep_sleep and sleep:
+            last_time_string = display_sleep_state(screen, state, last_time_string)
 
         if (
             not deep_sleep
@@ -129,21 +151,45 @@ def main():
             deep_sleep = True
             screen.set_backlight(False)
 
-        if distsensor.is_close():
+        if distsensor.is_close() and deep_sleep:
             last_dist_was_close_ms = time.ticks_ms()
             screen.set_backlight(True)
             deep_sleep = False
 
+        # Check if it's time to wake up
+        if should_wake_up(state):
+            print("Waking up!")
+            while not button.is_pressed():
+                time.sleep_ms(100)
+
+        print(f"Sleep: {sleep}, Deep Sleep: {deep_sleep}, Armed: {state.armed()}, Choice: {state.choice()}     ", end="\r")
         time.sleep_ms(50)
 
+def should_wake_up(state: AlarmState) -> bool:
+    """
+    Check if the current time matches the alarm time and the alarm is on.
+    """
+    if not state.armed():
+        return False
+    current_hour = time.localtime()[3]  # Hour
+    current_minute = time.localtime()[4]  # Minute
+    if (state.is_on()
+        and state.ms_since_last_change() > ALARM_STATE_CHANGE_FREEZE_MS
+        and state.hour() == current_hour
+        and state.minute() == current_minute):
+        state.just_fired()
+        return True
+    return False
 
-def display_sleep_state(screen, state):
+def display_sleep_state(screen, state, last_time_string):
     msg_time = time_string(include_seconds=False, prefix_day_of_week=True)
     msg_alarm = (
         f"Wake at {state.hour():02}:{state.minute():02}" if state.is_on() else ""
     )
     msg = "\n".join([msg_time, msg_alarm])
-    screen.message(msg, center=True)
+    if msg != last_time_string:
+        screen.message(msg, center=True)
+    return msg
 
 
 def display_alarm_state(screen, state):
@@ -169,10 +215,13 @@ def display_alarm_state(screen, state):
     screen.set_cursor_position(row, cursor_col)
 
 
-def update_alarm_state(pot, button, state):
+def update_alarm_state(pot, button, state, sleep):
     if not button.is_held():
         choice = pot.read_discrete(6) % 3
         state.choice(choice)
+
+    if sleep:
+        return
 
     if state.choice() == 0:
         if button.is_pressed():
